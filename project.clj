d9ccf32707e3f84612c78bdfa1881f86893e8d2c
NikolaF94 <nikolafilipovic4@gmail.com>
2019-03-01 16:33:03 +0100

    Add files via upload

--- project.clj
+++ project.clj
@@ -3,6 +3,6 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"], [me.jhenrique/getoldtweets "1.1.0"], [damionjunk/nlp  "0.3.0"], [com.novemberain/monger "3.1.0"], [congomongo "1.0.1"]]
+  :dependencies [[org.clojure/clojure "1.8.0"], [me.jhenrique/getoldtweets "1.1.0"], [incanter "1.9.3"], [damionjunk/nlp  "0.3.0"], [com.novemberain/monger "3.1.0"], [congomongo "1.0.1"], [clj-time "0.15.0"], [cheshire "5.8.1"]]
   :repositories {"local" "file:C:\\Users\\Luka\\.m2\\repository"}
   )
